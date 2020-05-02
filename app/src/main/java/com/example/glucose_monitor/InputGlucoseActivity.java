d69041af3fcccce8cefe3b61d3f1c292a3dd5be8
Roman Asadov <roma-asadov@mail.ru>
2020-05-02 17:41:07 +0300

    Добавил активити ввода уровня глюкозы

---
+++ app/src/main/java/com/example/glucose_monitor/InputGlucoseActivity.java
@@ -0,0 +1,14 @@
+package com.example.glucose_monitor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InputGlucoseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_glucose);
+    }
+}
--- app/src/main/java/com/example/glucose_monitor/MainActivity.java
+++ app/src/main/java/com/example/glucose_monitor/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.glucose_monitor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -22,5 +24,15 @@ public class MainActivity extends AppCompatActivity {
         notificationsBtn = (Button)findViewById(R.id.notifications_btn);
         forecastedNumTV = (TextView)findViewById(R.id.forecasted_num_tv);
         forecastedTimeTV = (TextView)findViewById(R.id.forecasted_time_tv);
+
+        saveValueBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent inputGlucoseActivity = new Intent(MainActivity.this, InputGlucoseActivity.class);
+                startActivity(inputGlucoseActivity);
+                finish();
+            }
+        });
+
     }
 }
