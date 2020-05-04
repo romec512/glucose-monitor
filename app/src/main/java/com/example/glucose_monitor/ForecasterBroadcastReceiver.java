ba6d8a671d10cac5513d4c4f9aa7c24a50fd1d1e
Roman Asadov <roma-asadov@mail.ru>
2020-05-04 19:06:48 +0300

    Added empty alarm for creating notifications and getting prediction

---
+++ app/src/main/java/com/example/glucose_monitor/ForecasterBroadcastReceiver.java
@@ -0,0 +1,17 @@
+package com.example.glucose_monitor;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import Classes.ArffHelper;
+
+public class ForecasterBroadcastReceiver extends BroadcastReceiver {
+    public static int Num = 0;
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+//        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
--- app/src/main/java/com/example/glucose_monitor/MainActivity.java
+++ app/src/main/java/com/example/glucose_monitor/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.glucose_monitor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -31,6 +35,18 @@ public class MainActivity extends AppCompatActivity {
                 Intent inputGlucoseActivity = new Intent(MainActivity.this, InputGlucoseActivity.class);
                 startActivity(inputGlucoseActivity);
                 finish();
+            }
+        });
+
+        notificationsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getBaseContext();
+                AlarmManager alarmManager = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+                Intent intent = new Intent(context, ForecasterBroadcastReceiver.class);
+                PendingIntent alarmIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
+                alarmManager.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                        SystemClock.elapsedRealtime() + 1000, 60*1000, alarmIntent);
             }
         });
 
