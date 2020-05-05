229a14e44dadd339be20f38ef3e025d4f9a7ff4f
Roman Asadov <roma-asadov@mail.ru>
2020-05-05 21:55:02 +0300

    Display forecast and its time on the main screen from file with predicted values

--- app/src/main/java/com/example/glucose_monitor/MainActivity.java
+++ app/src/main/java/com/example/glucose_monitor/MainActivity.java
@@ -12,6 +12,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
     private Button saveValueBtn;
     private Button graphicsBtn;
@@ -28,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
         notificationsBtn = (Button)findViewById(R.id.notifications_btn);
         forecastedNumTV = (TextView)findViewById(R.id.forecasted_num_tv);
         forecastedTimeTV = (TextView)findViewById(R.id.forecasted_time_tv);
+
+        setPrediction();
 
         saveValueBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,5 +60,43 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    protected void setPrediction() {
+        FileInputStream fin = null;
+        byte[] bytes = null;
+        try {
+            fin = new FileInputStream(new File("/storage/emulated/0/glucose-monitor/predicted.txt"));
+            bytes = new byte[fin.available()];
+            fin.read(bytes);
+        }catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if(fin != null) {
+                    fin.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if (bytes != null) {
+            String fileContent = new String(bytes);
+            String[] predictions = fileContent.split("\n");
+            String[] prediction = predictions[0].split(",");
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+            Date predictedDate = null;
+            try {
+                predictedDate = format.parse(prediction[0]);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm");
+            Float predicted = Float.parseFloat(prediction[1]) / 18;
+            forecastedNumTV.setText(String.format("%.2f", predicted));
+            forecastedTimeTV.setText("Ожидается в " + timeFormat.format(predictedDate));
+        }
     }
 }
